e4ba515aa8be12f40348532992dedb43a82200d5
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-09-27 09:14:24 -0300

    aws cli

--- modules/ec2-instance/userdata.sh
+++ modules/ec2-instance/userdata.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-su ec2-user
+su ubuntu
 
 export DEBIAN_FRONTEND=noninteractive
 export NEEDRESTART_MODE=a
@@ -7,10 +7,14 @@ export NEEDRESTART_MODE=a
 sudo apt update
 sudo apt upgrade -y
 
-sudo apt install -y nfs-common
+sudo apt install -y unzip nfs-common
+
+# AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
 
 # EFS mount
-
 unencryptedDnsName=$(aws ssm get-parameter --name /efs/unencrypted/dnsname --query Parameter.Value --output text)
 encryptedDnsName=$(aws ssm get-parameter --name /efs/encrypted/dnsname --query Parameter.Value --output text)
 
